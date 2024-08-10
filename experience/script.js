54b094492a46a0ac44c43cd1c2ae02ec027305d9
Niruta Poudel <pdiniruta1@gmail.com>
2024-08-10 18:03:01 +0545

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Niruta Poudel";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
